9d9c31bcb37cbee4a650d92bcbb638583b239091
keonoh00 <keonoh00@gmail.com>
2021-12-17 01:06:52 +0100

    Added Create Account Validation Feature

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -5,11 +5,23 @@ export const createAccount = (req, res) => {
 };
 
 export const postAccount = async (req, res) => {
-  const { name, email, username, password, location } = req.body;
+  const { name, email, password, password2, location } = req.body;
+  if (password !== password2) {
+    return res.render("createAccount", {
+      pageTitle: "Create Account",
+      errorMessage: "Passwords Confirmation Does Not Match",
+    });
+  }
+  const exists = await userDB.exists({ email });
+  if (exists) {
+    return res.render("createAccount", {
+      pageTitle: "Create Account",
+      errorMessage: "This email is already taken",
+    });
+  }
   await userDB.create({
     name,
     email,
-    username,
     password,
     location,
   });
--- src/models/user.js
+++ src/models/user.js
@@ -3,7 +3,6 @@ import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
-  username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   name: { type: String, required: true },
   location: { type: String },
